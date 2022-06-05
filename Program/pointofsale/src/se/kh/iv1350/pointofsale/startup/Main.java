2ad7f151aa1dc9e7d9349641a00c54bf7e073071
Geanja <andre.eriksson2@gmail.com>
2022-06-05 02:01:13 +0200

    Project

--- Program/pointofsale/src/se/kh/iv1350/pointofsale/startup/Main.java
+++ Program/pointofsale/src/se/kh/iv1350/pointofsale/startup/Main.java
@@ -24,13 +24,12 @@ public class Main {
         View view = new View(contr);
         String answerForSale = "";
         String shutDownSystem = "no";
-
         while(!shutDownSystem.equals("yes")) {
             contr.startSale();
             answerForSale = "";
             while (!answerForSale.equals("terminate")) {
                 Scanner sc = new Scanner(System.in);
-                System.out.println("press 1 for meatball \npress 2 for pringles \nwrite terminate to end sale ");
+                startProgramMenuUI();
                 answerForSale = sc.nextLine();
 
                 switch (answerForSale) {
@@ -39,10 +38,10 @@ public class Main {
                             contr.addItemToSale(1);
                         } catch (ItemNotFoundException e) {
                             System.out.println(e.getMessage());
-                            System.out.println("LOG: ItemNotFoundException was thrown due to " + e.getMessage());
+                            itemNotFoundLog(e);
                         } catch (ServerConnectionFailException p) {
                             System.out.println(p.getMessage());
-                            System.out.println("LOG: Could not add item to sale due to server being down");
+                            serverConnectionFailedLog(p);
                         }
                         break;
 
@@ -51,10 +50,10 @@ public class Main {
                             contr.addItemToSale(2);
                         } catch (ItemNotFoundException e) {
                             System.out.println(e.getMessage());
-                            System.out.println("LOG: ItemNotFoundException was thrown due to " + e.getMessage());
+                            itemNotFoundLog(e);
                         } catch (ServerConnectionFailException p) {
                             System.out.println(p.getMessage());
-                            System.out.println("LOG: Could not add item to sale due to server being down");
+                            serverConnectionFailedLog(p);
                         }
                         break;
 
@@ -63,10 +62,10 @@ public class Main {
                             contr.addItemToSale(3);
                         } catch (ItemNotFoundException e) {
                             System.out.println(e.getMessage());
-                            System.out.println("LOG: ItemNotFoundException was thrown due to " + e.getMessage());
+                            itemNotFoundLog(e);
                         } catch (ServerConnectionFailException p) {
                             System.out.println(p.getMessage());
-                            System.out.println("LOG: Could not add item to sale due to server being down");
+                            serverConnectionFailedLog(p);
                         }
 
                         break;
@@ -75,15 +74,14 @@ public class Main {
                         int totalPriceWithTax = contr.endSale();
                         int totalPayment = 0;
 
-                        System.out.println("It costs " + totalPriceWithTax + " kr");
-                        System.out.println("How much will you pay");
+                        askForPaymentPrint(totalPriceWithTax);
                         int payment = sc.nextInt();
                         totalPayment += payment;
                         if (payment < totalPriceWithTax)
                         {
                             totalPriceWithTax -= payment;
                             while (totalPriceWithTax > 0) {
-                                System.out.println("Inserted payment is not enough, please insert more money");
+                                paymentNotEnoughPrint();
                                 payment = sc.nextInt();
                                 totalPayment += payment;
                                 totalPriceWithTax -= payment;
@@ -96,20 +94,48 @@ public class Main {
 
                     default:
                         try {
-                            contr.addItemToSale(0);
+                            contr.addItemToSale(Integer.parseInt(answerForSale));
                         } catch (ItemNotFoundException e) {
                             System.out.println(e.getMessage());
-                            System.out.println("LOG: ItemNotFoundException was thrown due to " + e.getMessage());
+                            itemNotFoundLog(e);
                         } catch (ServerConnectionFailException p) {
                             System.out.println(p.getMessage());
-                            System.out.println("LOG: Could not add item to sale due to server being down");
+                            serverConnectionFailedLog(p);
                         }
                         break;
                 }
             }
-            System.out.println("Do you want to shut down the system? Write yes or no");
+            shutDownSystemQueryPrint();
             Scanner stringScanner = new Scanner(System.in);
             shutDownSystem = stringScanner.nextLine();
         }
     }
+
+    public static void startProgramMenuUI() {
+        System.out.println("press 1 for meatball \npress 2 for pringles \nwrite terminate to end sale ");
+    }
+
+    public static void itemNotFoundLog(ItemNotFoundException e) {
+        System.out.println("LOG: ItemNotFoundException was thrown due to " + e.getMessage());
+        System.out.println("LOG: stacktrace: "+ e.getStackTrace());
+    }
+
+    public static void serverConnectionFailedLog(ServerConnectionFailException p) {
+        System.out.println("LOG: Could not add item to sale due to server being down");
+        System.out.println("LOG: stacktrace: "+ p.getStackTrace());
+    }
+
+    public static void askForPaymentPrint(int totalPriceWithTax) {
+        System.out.println("It costs " + totalPriceWithTax + " kr");
+        System.out.println("How much will you pay");
+    }
+
+    public static void paymentNotEnoughPrint() {
+        System.out.println("Inserted payment is not enough, please insert more money");
+    }
+
+    public static void shutDownSystemQueryPrint() {
+        System.out.println("Do you want to shut down the system? Write yes or no");
+    }
+
 }
